0e18e7ef4e644022c9422269acc50556e263179d
Vũ Minh Tuấn <tuanvmth1911008@gmail.com>
2020-04-25 08:39:05 +0700

    test

---
+++ eproject_dnbc/index.js
@@ -0,0 +1,27 @@
+var express = require("express");
+
+var app = express();
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT,function () {
+    console.log("Server is running..");
+});
+
+app.use(express.static('public'));
+
+app.set("view engine","ejs");
+// var mssql = require("mssql");//goi module de su dung
+// const config = {
+//     server: "210.245.95.62",
+//     user: "sa",
+//     password: "z@GH7ytQ",
+//     database: "T1911E"
+// };
+// mssql.connect(config,function (err) {
+//     if (err) console.log(err);
+// });
+// var db = new mssql.Request();
+
+app.get("/",function (req,res) {
+    res.render("homepage");
+});
