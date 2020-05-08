dd5961397b0d12a13892bbde60c87541f370ebf9
tuanvmth1911008 <tuanvmth1911008@fpt.edu.vn>
2020-05-08 21:02:54 +0700

    fix

---
+++ public/js/ourstory.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+    $(window).scroll(function(event) {
+        var pos_body = $('html,body').scrollTop();
+        var pos_str1 = $('.str-content .str1').offset().top;
+        var pos_str2 = $('.str-content .str2').offset().top;
+        var pos_str3 = $('.str-content .str3').offset().top;
+        var pos_str4 = $('.str-content .str4').offset().top;
+        console.log(pos_str2);
+        if(pos_body>pos_str1-700){
+            $('.str-content .str1').addClass('animate__backInRight animate__animated');
+        }
+        if(pos_body>pos_str2-700){
+            $('.str-content .str2').addClass('animate__backInRight animate__animated');
+        }
+        if(pos_body>pos_str3-700){
+            $('.str-content .str3').addClass('animate__backInRight animate__animated');
+        }
+        if(pos_body>pos_str4-700){
+            $('.str-content .str4').addClass('animate__backInRight animate__animated');
+        }
+    });
+});
